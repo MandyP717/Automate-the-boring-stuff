2bdf5ca479c9686144271d4439462e601a64069c
MandyP717 <81747432+MandyP717@users.noreply.github.com>
2021-06-29 21:04:11 +0200

    Added more handling of hybrid list.

--- Comma code.py
+++ Comma code.py
@@ -1,18 +1,29 @@
 """
 Write a function that takes a list value as an argument and returns a string with all
-the items sperated by a comma and a space, with and inserted before the last item.
+the items seperated by a comma and a space, with and inserted before the last item.
 """
 
 
 def comma_code(join_list):
-    try:
-        join_list[-1] = f'and {join_list[-1]}'
-        comma =  ", ".join(join_list)
-    except:
-        comma = join_list
-    return comma
+    if len(join_list) > 2:
+        convert_list = [str(item) for item in join_list]
+        joined_string = ", ".join(convert_list[:-1])
+        return f"{joined_string} and {convert_list[-1]}"
+    elif len(join_list) == 2:
+        my_string = f"{join_list[0]} and {join_list[1]}"
+        return my_string
+    else:
+        return join_list
+
 
 spam = ["apples", "bananas", "tofu", "cats"]
 spam2 = []
+spam3 = ["apples", "bananas"]
+spam4 = ["apples", 3, "bananas", 4, "!"]
+spam5 = ['apples']
+
 print(comma_code(spam))
 print(comma_code(spam2))
+print(comma_code(spam3))
+print(comma_code(spam4))
+print(comma_code(spam5))
