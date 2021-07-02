cca20059c297187097d020d94412aff0f2ab32da
MandyP717 <81747432+MandyP717@users.noreply.github.com>
2021-07-02 21:20:05 +0200

    First draft.

---
+++ Chapter 6/Table Printer.py
@@ -0,0 +1,25 @@
+"""Write a function named print_table() that takes a list of lists of strings
+and displays it in a well-organized table with each column right-justified. 
+Assume that all the inner lists will contain the same number of strings."""
+
+table_data = [
+    ["apples", "oranges", "cherries", "banana"],
+    ["Alice", "Bob", "Carol", "David"],
+    ["dogs", "cats", "moose", "goose"],
+]
+
+
+def length_inner_list(table):
+    max_length = []
+    for inner_list in table_data:
+        length_string = max([len(item) for item in inner_list])
+        max_length.append(length_string)
+    return max_length
+
+def rjust_table(table):
+    transpose_list = [list(i) for i in zip(*table_data)]
+    for row in transpose_list:
+        print (' '.join(item.rjust(width) for item, width in zip(row, length_inner_list(table))))
+
+
+rjust_table(table_data)
