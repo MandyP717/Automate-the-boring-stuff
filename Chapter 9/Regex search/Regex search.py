9da3b74c3045dae79efe1fad8f6535b10c1ef604
MandyP717 <81747432+MandyP717@users.noreply.github.com>
2021-08-27 23:39:10 +0200

    setting up practice project

---
+++ Chapter 9/Regex search/Regex search.py
@@ -0,0 +1,17 @@
+"""
+Regex Search
+Write a program that opens all .txt files in a folder and searches for any
+line that matches a user-supplied regular expression. The results should
+be printed to the screen.
+"""
+
+import re
+from pathlib import Path
+
+# Get all .txt files from current directory
+# Put all found files in a list
+# Ask user for a regular expression
+# Loop through all the files to find the supplied expression
+# Save all found result to a list
+# Print the list to show the result
+
