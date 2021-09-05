7a020608688e34bc2cfa0f0b34ef5d4b36bb5c42
MandyP717 <81747432+MandyP717@users.noreply.github.com>
2021-09-05 15:33:14 +0200

    Finish project.

--- Chapter 9/Regex search/Regex search.py
+++ Chapter 9/Regex search/Regex search.py
@@ -8,11 +8,11 @@ be printed to the screen.
 import re
 from pathlib import Path, PurePath
 import glob
+import pprint
 
 # Get all .txt files from current directory
-path = str(Path.cwd() / "*.txt")
-
 # Put all found files in a list
+path = str(Path.cwd() / "*.txt")
 txt_file = glob.glob(path)
 
 # Ask user for a regular expression
@@ -20,13 +20,16 @@ search_term = input("What are you searching for? \n")
 # Loop through all the files to find the supplied expression
 found_search_results = {}
 for x in txt_file:
+    found_search_results[PurePath(x).name] = []
     with open(x, "r") as f:
-        read_stuff = f.read()
-        search_results = re.findall(search_term, read_stuff)
-        found_search_results[PurePath(x).name] = search_results
+        for paragraph in f:
+            data = paragraph.split(". ")
+            for line in data:
+                if re.search(rf"\b{search_term}\b", line):
+                    found_search_results[PurePath(x).name].append(line)
         f.close()
 
 
 # Save all found result to a list
 # Print the list to show the result
-print(found_search_results)
+pprint.pprint(found_search_results)
